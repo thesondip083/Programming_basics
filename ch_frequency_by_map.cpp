8153ace7db7fbc967d3c0140c9b8b443737b6e79
sondip poul singh <poulsingh083@gmail.com>
2019-05-05 11:33:02 +0600

    Update ch_frequency_by_map.cpp

--- ch_frequency_by_map.cpp
+++ ch_frequency_by_map.cpp
@@ -1,3 +1,10 @@
+/*
+map is first key lookup data structure and the key cant be duplicated,value associate with the keys can be the same or different.
+by using this criteria "every character of a string apperas in map only once" we can find the frequency of them by increasing the value
+every time same character appears.
+*/
+
+
 #include<iostream>
 #include<map>
 using namespace std;
